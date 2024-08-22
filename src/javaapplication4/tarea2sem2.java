2c29a25e7c47792265ae41b30d51c940d21a2071
Yefersson <Yefersson@Yefersson>
2024-08-22 15:33:56 -0500

    ejemplo uso de netbeans

---
+++ src/javaapplication4/tarea2sem2.java
@@ -0,0 +1,13 @@
+package javaapplication4;
+
+
+import java.util.Scanner;
+
+
+public class tarea2sem2 {
+    public static void main(String[] args) {
+        Scanner leer=new Scanner (System.in);
+        
+    }
+    
+}
